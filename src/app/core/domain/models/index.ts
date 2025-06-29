edeb8acd79849b65bde98e715f7ec02a0982be96
Gersomsim <gersomhernandez@me.com>
2025-06-29 16:48:36 -0600

    ✨ feat: agregar modelos de dominio para la app

---
+++ src/app/core/domain/models/index.ts
@@ -0,0 +1,4 @@
+export * from './task.model';
+export * from './category.model';
+export * from './tag.model';
+export * from './comment.model';
---
+++ src/app/core/domain/models/task.model.ts
@@ -0,0 +1,20 @@
+import { PriorityTask } from '../types/priority-task.type';
+import { Category } from './category.model';
+import { Comment } from './comment.model';
+import { Tag } from './tag.model';
+
+export interface Task {
+  id: string;
+  title: string;
+  description: string;
+  completed: boolean;
+  priority: PriorityTask;
+  category: Category;
+  dueDate?: Date;
+  tags?: Tag[];
+  attachments?: File[];
+  comments?: Comment[];
+  subtasks?: Task[];
+  createdAt?: Date;
+  updatedAt?: Date;
+}
