85964fbbbc9a7baa4f474987d03723ddcd70043e
Gersomsim <gersomhernandez@me.com>
2025-06-30 12:15:20 -0600

    🔧 fix: añadir proveedor de cliente HTTP y decorador en store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { TOKEN_CONFIG } from '@infrastructure/di/token.config';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
     ...TOKEN_CONFIG,
   ],
 };
--- src/app/infrastructure/state/stores/task.store.ts
+++ src/app/infrastructure/state/stores/task.store.ts
@@ -1,9 +1,11 @@
+import { Injectable } from '@angular/core';
 import { BaseStore } from './common/base.store';
 import { Task } from '@core/domain/models/task.model';
 
 /**
  * Store de Elf para el estado de las tareas
  */
+@Injectable({ providedIn: 'root' })
 export class TaskStore extends BaseStore<Task> {
   constructor() {
     super('task');
