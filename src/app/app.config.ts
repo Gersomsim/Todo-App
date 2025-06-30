191008aa8896521fb6738579625fb8f00db815bd
Gersomsim <gersomhernandez@me.com>
2025-06-30 12:04:03 -0600

    🔧 fix: actualizar configuración de app y servicio HTTP

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,12 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { TOKEN_CONFIG } from '@infrastructure/di/token.config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    ...TOKEN_CONFIG,
+  ],
 };
--- src/app/infrastructure/http/http.service.ts
+++ src/app/infrastructure/http/http.service.ts
@@ -8,30 +8,31 @@ import { generateQueryParams } from '../../utils';
   providedIn: 'root',
 })
 export class HttpService {
+  private readonly ApiUrl = 'http://localhost:3000/api/alfa/';
   constructor(private readonly httpClient: HttpClient) {}
 
   get<T>(url: string, filters?: any): Observable<T> {
     const params = generateQueryParams(filters);
-    return this.httpClient.get<T>(url, { params });
+    return this.httpClient.get<T>(`${this.ApiUrl}${url}`, { params });
   }
 
   post<T>(url: string, body: any, filters?: any): Observable<T> {
     const params = generateQueryParams(filters);
-    return this.httpClient.post<T>(url, body, { params });
+    return this.httpClient.post<T>(`${this.ApiUrl}${url}`, body, { params });
   }
 
   put<T>(url: string, body: any, filters?: any): Observable<T> {
     const params = generateQueryParams(filters);
-    return this.httpClient.put<T>(url, body, { params });
+    return this.httpClient.put<T>(`${this.ApiUrl}${url}`, body, { params });
   }
 
   patch<T>(url: string, body: any, filters?: any): Observable<T> {
     const params = generateQueryParams(filters);
-    return this.httpClient.patch<T>(url, body, { params });
+    return this.httpClient.patch<T>(`${this.ApiUrl}${url}`, body, { params });
   }
 
   delete<T>(url: string, filters?: any): Observable<T> {
     const params = generateQueryParams(filters);
-    return this.httpClient.delete<T>(url, { params });
+    return this.httpClient.delete<T>(`${this.ApiUrl}${url}`, { params });
   }
 }
