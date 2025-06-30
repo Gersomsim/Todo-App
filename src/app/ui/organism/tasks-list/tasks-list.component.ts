7c948e242c3a36706659f6828cbc1265ddc786be
Gersomsim <gersomhernandez@me.com>
2025-06-30 16:46:39 -0600

    ✨ feat: agregar componente tasks-list y su lógica

---
+++ src/app/ui/organism/tasks-list/tasks-list.component.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { Task } from '@core/domain/models';
+import { TaskFacade } from '@infrastructure/state/facades/task.facade';
+import { TaskComponent } from '@ui/molecules/task/task.component';
+
+@Component({
+  selector: 'app-tasks-list',
+  standalone: true,
+  imports: [TaskComponent, CommonModule],
+  templateUrl: './tasks-list.component.html',
+  styleUrl: './tasks-list.component.css',
+})
+export class TasksListComponent {
+  taskFacade = inject(TaskFacade);
+
+  tasks = signal<Task[]>([]);
+  loading = signal(false);
+
+  ngOnInit() {
+    this.taskFacade.items$.subscribe((tasks) => {
+      console.log('tasks', this.tasks(), tasks);
+
+      this.tasks.set(tasks);
+    });
+    this.taskFacade.loadAll();
+  }
+}
