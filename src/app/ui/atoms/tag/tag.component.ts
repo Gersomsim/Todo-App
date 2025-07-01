3d25328b1de8591393f7266ce5289ee506f9e1dc
Gersomsim <gersomhernandez@me.com>
2025-07-01 17:05:20 -0600

    ✨ feat: mejorar componentes de etiqueta y textarea

--- src/app/ui/atoms/tag/tag.component.ts
+++ src/app/ui/atoms/tag/tag.component.ts
@@ -1,25 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { Icon } from '@ui/types/icon.type';
+import { IconComponent } from '../icon/icon.component';
 
 @Component({
   selector: 'app-tag',
   standalone: true,
-  imports: [],
+  imports: [IconComponent],
   template: `
     <span class="flex items-center gap-1">
-      <svg
-        class="w-4 h-4"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-        ></path>
-      </svg>
+      @if (icon) {
+      <app-icon [icon]="icon" />
+      }
       {{ label }}
     </span>
   `,
