971049e0cbbee8b074c6df461a8c2ed969714a28
Gersomsim <gersomhernandez@me.com>
2025-07-02 06:28:08 -0600

    ✨ feat: mejorar lógica de filtrado en widget de productividad

--- src/app/ui/molecules/widget-productivity/widget-productivity.component.ts
+++ src/app/ui/molecules/widget-productivity/widget-productivity.component.ts
@@ -1,6 +1,7 @@
 import { Component, computed, inject, signal } from '@angular/core';
 import { Task } from '@core/domain/models';
 import { TaskFacade } from '@infrastructure/state/facades/task.facade';
+import { DateTime } from 'luxon';
 
 @Component({
   selector: 'app-widget-productivity',
@@ -12,20 +13,31 @@ import { TaskFacade } from '@infrastructure/state/facades/task.facade';
 export class WidgetProductivityComponent {
   taskFacade = inject(TaskFacade);
   workingTasks = signal<Task[]>([]);
-  today = new Date();
+  today = DateTime.now().toJSDate();
   taskToday = computed(() => {
-    return this.workingTasks().filter(
-      (task) => task.dueDate === this.today && !task.completed
-    );
+    return this.workingTasks().filter((task) => {
+      if (!task.dueDate) return false;
+      const taskDate = DateTime.fromJSDate(new Date(task.dueDate)).toFormat(
+        'yyyy-MM-dd'
+      );
+      const today = DateTime.fromJSDate(this.today).toFormat('yyyy-MM-dd');
+      return taskDate === today && !task.completed;
+    });
   });
   taskCompleted = computed(() => {
-    return this.workingTasks().filter(
-      (task) => task.completed && task.completedAt === this.today
-    );
+    return this.workingTasks().filter((task) => {
+      if (!task.completedAt) return false;
+      const taskDate = DateTime.fromJSDate(new Date(task.completedAt)).toFormat(
+        'yyyy-MM-dd'
+      );
+      const today = DateTime.fromJSDate(this.today).toFormat('yyyy-MM-dd');
+      return taskDate === today;
+    });
   });
 
   ngOnInit() {
     this.taskFacade.items$.subscribe((tasks) => {
+      console.log('tasks - widget', tasks);
       this.workingTasks.set(tasks);
     });
   }
